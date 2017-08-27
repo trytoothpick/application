f2b3f07114ca9c503fa319ad28a3d16d119221f4
Stephen Chen <hello@stephenc.me>
2017-08-26 22:22:06 -0700

    Fixes some bugs with truffle-contract code

--- app/App.js
+++ app/App.js
@@ -4,7 +4,10 @@ import { Text, View } from 'react-native';
 import Ethereum from './utils/ethereumController';
 
 export default class App extends Component {
-  render = () => (
-    <Text>Hello world!</Text>
-  );
+  render() {
+    return (
+      <Text>Hello world!</Text>
+
+    );
+  }
 }
--- app/utils/ethereumController.js
+++ app/utils/ethereumController.js
@@ -3,24 +3,31 @@ import TruffleContract from 'truffle-contract';
 
 let instance = null;
 
-const URL = 'http://localhost';
-const PORT = 8545;
+const URL = 'http://localhost:8545';
 
-export default class Ethereum {
+class Ethereum {
   constructor() {
-    if (!instance) {
-      if (typeof this.web3 !== 'undefined') {
-        this.web3Provider = this.web3.currentProvider;
-      } else {
-        this.web3Provider = new this.Web3.providers.HttpProvider(URL + ':' + PORT);
-      }
-
-      this.web3 = new Web3(this.web3Provider);
-
-      instance = this;
+    // initialize web3
+    if (typeof this.web3 !== 'undefined') {
+      this.web3Provider = this.web3.currentProvider;
+    } else {
+      this.web3Provider = new Web3.providers.HttpProvider(URL);
     }
 
-    return instance;
-  }
+    this.web3 = new Web3(this.web3Provider);
 
+    // initialize contract
+    fetch(URL + '/Adoption.json').then((data) => {
+      // // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      // var AdoptionArtifact = data;
+      // App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      //
+      // // Set the provider for our contract.
+      // App.contracts.Adoption.setProvider(App.web3Provider);
+      //
+      // // Use our contract to retieve and mark the adopted pets.
+      // return App.markAdopted();    });
+  }
 }
+
+export default new Ethereum();
