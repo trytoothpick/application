fcefe89b80c1e589bd861a391726a20158b583c5
Stephen Chen <hello@stephenc.me>
2017-08-26 23:50:55 -0700

    Adds StackNavigators to Advertiser and Influencer components

--- app/Advertiser/index.js
+++ app/Advertiser/index.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class AdvertiserRoot extends Component {
+class Advertiser extends Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
     return <Text>AdvertiserRoot</Text>;
   }
 }
+
+export default StackNavigator({
+  Root: { screen: Advertiser },
+});
--- app/Influencer/index.js
+++ app/Influencer/index.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class InfluencerRoot extends Component {
+class Influencer extends Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
     return <Text>InfluencerRoot</Text>;
   }
 }
+
+export default StackNavigator({
+  Root: { screen: Influencer },
+});
