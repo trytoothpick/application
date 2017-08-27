d061b178eab54b44a4ab6d58af11186eedab01d7
Stephen Chen <hello@stephenc.me>
2017-08-27 03:39:13 -0700

    Adds basic feed to Influencer

--- app/Influencer/screens/Feed.js
+++ app/Influencer/screens/Feed.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import { stylesVar } from '../../common.styles';
 
@@ -7,15 +7,59 @@ export default class Feed extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: 'Feed',
   });
+  renderListItem() {
+    return (
+      <View style={styles.listItemContainer}>
+        <View style={styles.liHeader}>
+          <Text style={[styles.text, styles.headerText]}>0.4 ETH</Text>
+        </View>
+        <View style={styles.liFooter}>
+          <Text style={styles.text}>1 mile away</Text>
+          <Text style={[styles.text, styles.titleText]}>Bread & Basket</Text>
+          <Text style={styles.text}>Address</Text>
+        </View>
+      </View>
+    );
+  }
   render() {
     const { params } = this.props.navigation.state;
 
     return (
-      <View></View>
+      <ScrollView>
+        {this.renderListItem()}
+        {this.renderListItem()}
+        {this.renderListItem()}
+        {this.renderListItem()}
+        {this.renderListItem()}
+        {this.renderListItem()}
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  text: {
+    color: 'white',
+  },
+  headerText: {
+    fontWeight: 'bold',
+  },
+  titleText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  listItemContainer: {
+    marginTop: 8,
+    padding: 16,
+    height: 140,
+    backgroundColor: 'gray',
+  },
+  liHeader: {
 
+  },
+  liFooter: {
+    position: 'absolute',
+    bottom: 16,
+    left: 16,
+  }
 });
